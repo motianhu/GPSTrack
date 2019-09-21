5cf91a901b7854a7a5006e6191feae2fd2ffe744
moth <moth@gionee.com>
2019-09-22 01:17:24 +0800

    设备详情

---
+++ app/src/main/java/com/smona/gpstrack/device/DeviceAddActivity.java
@@ -0,0 +1,24 @@
+package com.smona.gpstrack.device;
+
+import com.smona.base.ui.activity.BasePresenterActivity;
+import com.smona.gpstrack.R;
+import com.smona.gpstrack.device.presenter.DeviceAddPresenter;
+import com.smona.http.wrapper.ErrorInfo;
+
+public class DeviceAddActivity extends BasePresenterActivity<DeviceAddPresenter, DeviceAddPresenter.IDeviceAddView> implements DeviceAddPresenter.IDeviceAddView
+{
+    @Override
+    protected DeviceAddPresenter initPresenter() {
+        return new DeviceAddPresenter();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_device_add;
+    }
+
+    @Override
+    public void onError(String api, int errCode, ErrorInfo errorInfo) {
+
+    }
+}
---
+++ app/src/main/java/com/smona/gpstrack/device/presenter/DeviceAddPresenter.java
@@ -0,0 +1,10 @@
+package com.smona.gpstrack.device.presenter;
+
+import com.smona.base.ui.mvp.BasePresenter;
+import com.smona.gpstrack.common.IView;
+
+public class DeviceAddPresenter extends BasePresenter<DeviceAddPresenter.IDeviceAddView> {
+    public interface IDeviceAddView extends IView{
+
+    }
+}
