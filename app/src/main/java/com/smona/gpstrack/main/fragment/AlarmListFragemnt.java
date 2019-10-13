b48b9eaa161f5c54dfcfbdbe9fe4e0a2e3bb38f0
moth <moth@gionee.com>
2019-10-13 09:56:56 +0800

    修改navigation高度

--- app/src/main/java/com/smona/gpstrack/main/fragment/AlarmListFragemnt.java
+++ app/src/main/java/com/smona/gpstrack/main/fragment/AlarmListFragemnt.java
@@ -1,5 +1,6 @@
 package com.smona.gpstrack.main.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
@@ -9,6 +10,8 @@ import com.smona.gpstrack.alarm.presenter.AlarmListPresenter;
 import com.smona.gpstrack.component.WidgetComponent;
 import com.smona.gpstrack.db.table.Alarm;
 import com.smona.gpstrack.main.adapter.AlarmAdapter;
+import com.smona.gpstrack.util.ToastUtil;
+import com.smona.gpstrack.widget.adapter.RecycleViewDivider;
 import com.smona.http.wrapper.ErrorInfo;
 
 import java.util.List;
@@ -40,6 +43,8 @@ public class AlarmListFragemnt extends BasePresenterFragment<AlarmListPresenter,
         XRecyclerView recyclerView = content.findViewById(R.id.xrecycler_wiget);
         mAdapter = new AlarmAdapter(R.layout.adapter_item_alarm);
         recyclerView.setAdapter(mAdapter);
+        recyclerView.addItemDecoration(new RecycleViewDivider(
+                mActivity, LinearLayoutManager.VERTICAL, 10, getResources().getColor(R.color.white)));
 
         WidgetComponent.initXRecyclerView(mActivity, recyclerView, new XRecyclerView.LoadingListener() {
 
@@ -57,11 +62,12 @@ public class AlarmListFragemnt extends BasePresenterFragment<AlarmListPresenter,
     @Override
     protected void initData() {
         super.initData();
+        mPresenter.requestAlarmList();
     }
 
     @Override
     public void onError(String api, int errCode, ErrorInfo errorInfo) {
-
+        ToastUtil.showShort(errorInfo.getMessage());
     }
 
     @Override
