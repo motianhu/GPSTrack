9e6e0cd2cf01c117cabdd4968f2cc27f2c645656
Motianhu <motianhu@evergrande.com>
2019-12-31 15:56:02 +0800

    部分翻译

--- app/src/main/java/com/smona/gpstrack/map/IMap.java
+++ app/src/main/java/com/smona/gpstrack/map/IMap.java
@@ -6,24 +6,46 @@ import com.smona.gpstrack.fence.bean.FenceBean;
 import java.util.List;
 
 public interface IMap {
+    /**
+     * 实时定位接口
+     */
+    //地图聚焦到坐标点
     void animateCamera(double defaultLa, double defaultLo);
+    //绘制marker
     void drawMarker(double centerLa, double centerLo);
+    //清除地图
     void clear();
+    //获取当前位置坐标
     double[] getCurLocation();
 
-    //navigation
+    /**
+     * 导航接口
+     */
+    //初始化导航
     void initSearch(int type, double targetLa, double targetLo);
-    void refreshRoute();//重新绘制线路
-    void refreshDevice(double deviceLa, double deviceLo);//仅刷新设备位置
+    //重新绘制线路
+    void refreshRoute();
+    //仅刷新设备位置
+    void refreshDevice(double deviceLa, double deviceLo);
+    //移除导航
     void removeSearch();
 
-    //history
+    /**
+     * 历史路线接口
+     */
+    //绘制路线
     void drawTrack(List<Location> points);
 
-    ////电子围栏需要的接口///
+    /**
+     * 电子围栏需要的接口
+     */
+    //监听地图点击
     void setOnMapClickListener();
-    void onAutoMapClick(FenceBean fenceBean);//初始化选点
+    //初始化选点
+    void onAutoMapClick(FenceBean fenceBean);
+    //设置半径
     void setRadius(int radius);
+    //获取经度纬度
     double getLatitude();
     double getLongitude();
 }
